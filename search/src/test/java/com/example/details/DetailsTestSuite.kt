ecf39aefc6006b9c97eb579bf28db9f7e8d95dc1
Partha Sutradhar <partha.sutradhar@mobimeo.com>
2023-09-03 22:09:00 +0200

    set up user details DI module

--- search/src/test/java/com/example/details/DetailsTestSuite.kt
+++ search/src/test/java/com/example/details/DetailsTestSuite.kt
@@ -2,6 +2,7 @@ package com.example.details
 
 import com.example.details.data.repository.UserDetailsRepositoryImplTest
 import com.example.details.domain.model.UserDetailsMapperKtTest
+import com.example.details.domain.usecase.GetUserDetailsUseCaseImplTest
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
 import org.junit.runners.Suite.SuiteClasses
@@ -9,6 +10,7 @@ import org.junit.runners.Suite.SuiteClasses
 @RunWith(Suite::class)
 @SuiteClasses(
     UserDetailsRepositoryImplTest::class,
-    UserDetailsMapperKtTest::class
+    UserDetailsMapperKtTest::class,
+    GetUserDetailsUseCaseImplTest::class,
 )
 class DetailsTestSuite
